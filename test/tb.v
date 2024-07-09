bc8f5134dd2fbf798da0646fa0e7cd9322ccb69d
Aba <abarajithan-7@gmail.com>
2024-07-08 22:10:24 -0700

    Fix title

--- test/tb.v
+++ test/tb.v
@@ -23,8 +23,8 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
-  // Replace tt_uart_mvm with your module name:
-  tt_uart_mvm user_project (
+  // Replace tt_um_uart_mvm with your module name:
+  tt_um_uart_mvm user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
