12077f255e93ac613a45b3c3b5797c61125a14a9
Aba <abarajithan07@gmail.com>
2024-08-31 02:05:41 -0700

    change wy to 8

--- test/mvm_uart_system_tb.sv
+++ test/mvm_uart_system_tb.sv
@@ -2,7 +2,7 @@ module mvm_uart_system_tb;
 
   timeunit 1ns/1ps;
   localparam  R=4, C=4, W_X=4, W_K=2,
-              W_Y_OUT          = 10,
+              W_Y_OUT          = 8,
               CLOCKS_PER_PULSE = 4, //200_000_000/9600,
               BITS_PER_WORD    = 8,
               W_Y              = W_X + W_K + $clog2(C),
