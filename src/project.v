110d85b1ef8e32502df4b59bea5b22a691c33c7f
Aba <abarajithan07@gmail.com>
2024-09-06 17:02:56 -0700

    baud=19200

--- src/project.v
+++ src/project.v
@@ -17,7 +17,7 @@ module tt_um_uart_mvm (
 );
 
   localparam 
-    CLOCKS_PER_PULSE = 33,
+    CLOCKS_PER_PULSE = 50_000_000/19200,
     BITS_PER_WORD    = 8                ,
     PACKET_SIZE_TX   = BITS_PER_WORD + 5,
     W_Y_OUT          = 8                ,
